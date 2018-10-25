f4ba1d2f69a65f5234e5bdeee7710381f11c20a3
FlorianBlt <Florianblot059@gmail.com>
2018-10-25 09:21:25 +0200

    Init gitflow

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('v2/api/doc', app, document);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 bootstrap();
